9f2f9c8eef8dc26fa25f0ffe4b8f5e492c3ea668
HaiderAli771 <r.haiderali222@gmail.com>
2024-06-30 16:19:02 +0500

    Add Margin in Bottom

--- src/app/Bottom.tsx
+++ src/app/Bottom.tsx
@@ -10,7 +10,7 @@ export default function Bottom() {
           <h1 className="text-4xl font-bold leading-relaxed tracking-normal pl-[21px] md:pl-0 md:pr-0 pr-[21px] ">Ultimate support  system for leading agencies </h1>
           <div className=" flex flex-col gap-9">
             <p className="leading-9 tracking-tighter pl-[21px] md:pl-0 md:pr-0 pr-[21px]  ">Get your test delivered at let home collect sample from the victory of managments that supplies best design system guidelines ever</p>
-            <div className="flex md:flex-row  flex-col  gap-3">
+            <div className="flex md:flex-row  ml-[21px] mr-[21px] md:m-0 flex-col  gap-3">
               <input className=" drop-shadow-lg md:w-[300px] p-3" type="email" name="email" placeholder="Enter your email" />
               <button className="bg-[#FFC059] text-white rounded-md p-2">Get started</button>
             </div>
