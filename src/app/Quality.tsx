f54542373d07dffe873c5ad5207b43da877deb43
littledemon1234 <141943563+littledemon1234@users.noreply.github.com>
2024-07-01 17:14:51 +0500

    Update Quality.tsx

--- src/app/Quality.tsx
+++ src/app/Quality.tsx
@@ -4,10 +4,9 @@ import Agency from './assets/agency.jpg'
 export default function Quality() {
   return (
     <div className="flex md:flex-row flex-col justify-center">
-
       <div className="bg-[#353448] pt-8 mt-10 md:h-[600px] ">
         <div className=" flex md:flex-row flex-col md:justify-center md:gap-28  md:mr-0  md:ml-0 ">
-          <div className="md:pl-20 pt-[20px] md:w-[500px]">
+          <div className="md:pl-20 pr-[21px] md:pr-0 pt-[20px] md:w-[500px]">
             <div >
               <h1 className="md:text-3xl text-xl font-bold leading-relaxed tracking-tight text-white ml-[21px]">Introduce quality feature that boost your website rank & performance</h1>
               <p className="pt-5 leading-8 mb-8 text-white md:text-lg text-sm ml-[21px]">Build an incredible workplace that grow your business with Gusto&apos;s all in one platform with amazing contents. Get your test delivered at home collect sample from the victory of supplies design  </p>
